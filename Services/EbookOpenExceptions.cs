82fe8eb780ba5daed07d5de8d9e5f510a59cf909
Kiyō Jo <x2425618950@gmail.com>
2026-08-16 21:35:58 +0800

    fix: use extensible base for DRM open exception

--- Services/EbookOpenExceptions.cs
+++ Services/EbookOpenExceptions.cs
@@ -1,6 +1,6 @@
 namespace PageArc.Services;
 
-public sealed class DrmProtectedEbookException : InvalidDataException
+public sealed class DrmProtectedEbookException : IOException
 {
     public DrmProtectedEbookException(string message) : base(message) { }
     public DrmProtectedEbookException(string message, Exception innerException) : base(message, innerException) { }
